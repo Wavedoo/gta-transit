82ea42bdcfd6ded74aaef8f3b0617ab4e86527b7
Sterling <sterling1@hotmail.ca>
2023-07-30 00:32:35 -0400

    Tested using TTC's API

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from flask import Flask, request, jsonify
+from urllib.request import urlopen
+
+import json
 
 app = Flask(__name__)
 
+BASE_URL = "https://myttc.ca/"
+
 @app.route("/")
 def home():
-    return "Home"
+    return "Welcome tester"
+
+@app.route("/near")
+def near():
+    long = request.args.get("longitude")
+    lat = request.args.get("latitude")
+
+    print("long", long)
+    print("lat", lat)
+
+
+    if long and lat:
+        url = BASE_URL +"near/"+long +"," +lat + ".json"
+        return "Trying to access " + url + ", but there are some issues. Look into GTFS."
+        # return processUrl(url), 200
+    else:
+        url = BASE_URL + "kennedy_station.json"
+        return processUrl(url), 200
+
 
 @app.route("/get-user/<user_id>")
 def get_user(user_id):
@@ -20,6 +43,16 @@ def get_user(user_id):
 
     return jsonify(user_data), 200
 
+def processUrl(url):
+    print(url)
+    # store the response of URL
+    response = urlopen(url)
+
+    # storing the JSON response 
+    # from url in data
+    data_json = json.loads(response.read())
+
+    return data_json
 
 if __name__ == "__main__":
     app.run(debug=True)
